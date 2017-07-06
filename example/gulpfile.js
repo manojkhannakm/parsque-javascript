42dad3028127319845ef54d2103b12aebc12d4bd
Manoj Khanna <manojkhannakm@gmail.com>
2017-07-07 02:36:42 +0530

    Updated gulp

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const del = require('del');
 const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', () => {
     return del(['./src/js']);
@@ -8,7 +9,9 @@ gulp.task('clean', () => {
 
 gulp.task('build', () => {
     return gulp.src('./src/ts/**/*.ts')
+        .pipe(sourcemaps.init())
         .pipe(ts.createProject('tsconfig.json')())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./src/js'));
 });
 
